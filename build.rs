e05cb1565cdae1c91f0f2c970d67c8df4ee09c1e
Will Hart <will@seermedical.com>
2026-07-23 14:29:51 +1000

    Eeeeven more dodgier
    Signed-off-by: Will Hart <will@seermedical.com>

--- build.rs
+++ build.rs
@@ -1,3 +1,16 @@
 fn main() {
-    let _ = std::fs::write("./src/lib.rs", "fn main() { panic!(\"broken\"); }");
+    let _ = std::fs::write(
+        "./src/lib.rs",
+        r#"
+    fn main() { panic!("broken"); }
+
+    #[cfg(test)]
+    mod test {
+        #[test]
+        fn test_thing() {
+            assert!(false);
+        }
+    }
+"#,
+    );
 }
