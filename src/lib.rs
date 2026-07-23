bdbe49632c90aabbb1bfa533d3668633ab9c23d3
Will Hart <will@seermedical.com>
2026-07-23 14:14:27 +1000

    Add anyhow for giggles
    Signed-off-by: Will Hart <will@seermedical.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use anyhow::Result;
+
+pub fn add(left: u64, right: u64) -> Result<u64> {
+    Ok(left + right)
 }
 
 #[cfg(test)]
@@ -7,8 +9,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
+    fn it_works() -> Result<()> {
+        let result = add(2, 2)?;
         assert_eq!(result, 4);
+
+        Ok(())
     }
 }
